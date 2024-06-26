7f4843a58f8f4aa8b73915c17af2a869d78f2393
Sameed Husayn <sameedhussain2001@gmail.com>
2024-06-26 17:12:26 +0500

    updated models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def init_model_and_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_name, load_in_8bit=True, trust_remote_code=True)
+    return tokenizer, model
