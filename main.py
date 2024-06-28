cd9682b72ffa332cde7e303c4ddaa07f3cf38bc7
Sameed Husayn <sameedhussain2001@gmail.com>
2024-06-28 13:35:26 +0500

    Updated

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-import inquirer
 from models import init_model_and_tokenizer
 from data import load_dataset_subset
 from evaluations import evaluate_hellaswag, evaluate_glue_cola, evaluate_glue_sst2, evaluate_glue_qqp, evaluate_glue_stsb, evaluate_dialogsum, evaluate_perplexity, evaluate_mmlu
 
+def print_task_choices():
+    print("Select an evaluation task:")
+    print("1. Hellaswag")
+    print("2. COLA")
+    print("3. SST-2")
+    print("4. QQP")
+    print("5. STSB")
+    print("6. DialogSum")
+    print("7. Perplexity")
+    print("8. MMLU")
+
 def main():
-    questions = [
-        inquirer.List(
-            'task',
-            message="Select an evaluation task",
-            choices=["hellaswag", "cola", "sst2", "qqp", "stsb", "dialogsum", "perplexity", "mmlu"],
-        ),
-        inquirer.Text('model_name', message="Enter the model name or path", default="microsoft/Phi-3-mini-4k-instruct")
-    ]
-    
-    answers = inquirer.prompt(questions)
-    model_name = answers['model_name']
-    task = answers['task']
+    print_task_choices()
+    task_choice = input("Enter the number of the task you want to evaluate: ")
 
-    tokenizer, model = init_model_and_tokenizer(model_name)
+    tasks = {
+        '1': evaluate_hellaswag,
+        '2': evaluate_glue_cola,
+        '3': evaluate_glue_sst2,
+        '4': evaluate_glue_qqp,
+        '5': evaluate_glue_stsb,
+        '6': evaluate_dialogsum,
+        '7': evaluate_perplexity,
+        '8': evaluate_mmlu
+    }
 
-    if task == "hellaswag":
-        evaluate_hellaswag(tokenizer, model)
-    elif task == "cola":
-        evaluate_glue_cola(tokenizer, model)
-    elif task == "sst2":
-        evaluate_glue_sst2(tokenizer, model)
-    elif task == "qqp":
-        evaluate_glue_qqp(tokenizer, model)
-    elif task == "stsb":
-        evaluate_glue_stsb(tokenizer, model)
-    elif task == "dialogsum":
-        evaluate_dialogsum(tokenizer, model)
-    elif task == "perplexity":
-        evaluate_perplexity(tokenizer, model)
-    elif task == "mmlu":
-        evaluate_mmlu(tokenizer, model)
+    task_number = str(task_choice)
+    if task_number in tasks:
+        model_name = input("Enter the model name or path (default: microsoft/Phi-3-mini-4k-instruct): ") or "microsoft/Phi-3-mini-4k-instruct"
+        tokenizer, model = init_model_and_tokenizer(model_name)
+        tasks[task_number](tokenizer, model)
+    else:
+        print("Invalid choice. Please enter a number between 1 and 8.")
 
 if __name__ == "__main__":
     main()
