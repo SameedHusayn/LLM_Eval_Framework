060659dec2dd3f5fa868700c98ae56886ee20b6e
Sameed Husayn <sameedhussain2001@gmail.com>
2024-06-28 11:41:29 +0500

    removed to.cpu

--- models.py
+++ models.py
@@ -2,5 +2,5 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def init_model_and_tokenizer(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(model_name, load_in_4bit=True, trust_remote_code=True).to("cpu")
+    model = AutoModelForCausalLM.from_pretrained(model_name, load_in_4bit=True, trust_remote_code=True)
     return tokenizer, model
