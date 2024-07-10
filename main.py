6a3ed1f16be5e3bc6b3d1b11717ecced4b13511b
unknown <sameedhussain2001@gmail.com>
2024-07-10 14:39:36 +0500

    Added Docker supprt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from models import init_model_and_tokenizer
 from huggingface_hub import notebook_login
 from constants import models, tasks
+from huggingface_hub import interpreter_login
+
 
 import os
 import shutil
@@ -24,8 +26,7 @@ def main():
         None
     """
 
-    notebook_login()
-    
+    interpreter_login()    
     print("Select models:")
     for model in models.values():
         print(model['description'])
@@ -42,14 +43,6 @@ def main():
     selected_tasks = [tasks[task.strip()]['function'] for task in task_choices if task.strip() in tasks]
 
     for model_name in selected_models:
-        # Path to the model directory, replace 'model_name' with the actual model name
-        model_dir = os.path.join('/root/.cache/huggingface/transformers/models--', model_name)
-        # Check if the directory exists and remove it
-        if os.path.exists(model_dir):
-            shutil.rmtree(model_dir)
-            print(f"Deleted model directory: {model_dir}")
-        else:
-            print(f"Model directory does not exist: {model_dir}")
         tokenizer, model = init_model_and_tokenizer(model_name)
         for task in selected_tasks:
             task(tokenizer, model)
