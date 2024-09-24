44e6d8cd69ea8e2c0b151c8c22aaaf026a11126f
Syed Sameed Hussain <62431458+SameedHusayn@users.noreply.github.com>
2024-09-24 17:04:00 +0500

    Update models.py

--- models.py
+++ models.py
@@ -19,5 +19,6 @@ def init_model_and_tokenizer(model_name):
   else:
     if tokenizer.pad_token is None:
       tokenizer.pad_token = tokenizer.eos_token
-  model = AutoModelForCausalLM.from_pretrained(model_name, load_in_4bit=True, trust_remote_code=True)
+  # model = AutoModelForCausalLM.from_pretrained(model_name, load_in_4bit=True, trust_remote_code=True)
+  model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
   return tokenizer, model
